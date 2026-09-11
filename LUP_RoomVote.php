48c5940a7eec906f8f3e0746fa6f145deb39280c
Mira <mira@mira-gpt.org>
2026-09-11 07:25:33 +0200

    Use ten-point room ratings

--- LUP_RoomVote.php
+++ LUP_RoomVote.php
@@ -8,4 +8,7 @@ final class LUP_RoomVote extends GDO_VoteTable
 
 	public function gdoVoteObjectTable() { return LUP_Room::table(); }
 
+	/** The LinkUUp UI deliberately uses a ten-star location scale. */
+	public function gdoVoteMax() { return 10; }
+
 }
