7e9259d78f7593608a8d8755811fda448096cb27
gizmore <gizmore@wechall.net>
2026-09-18 04:05:26 +0200

    'more korean'

--- Install.php
+++ Install.php
@@ -193,7 +193,7 @@ final class Install
 		Module_UI::instance()->saveConfigVar('default_editor', 'Text');
 		Module_UI::instance()->saveConfigVar('allow_editor_choice', '0');
 		Module_UI::instance()->saveConfigVar('store_device_info', '0');
-		Module_Language::instance()->saveConfigVar('languages', '["en","de","it","fr","es"]');
+		Module_Language::instance()->saveConfigVar('languages', '["en","de","it","fr","es","ko"]');
 		Module_Maps::instance()->saveConfigVar('maps_record_history', '60s');
         Module_Websocket::instance()->saveConfigVar('ws_processor', GDO_PATH . 'GDO/LinkUUp/LUP_Websocket.php');
         Module_Websocket::instance()->saveConfigVar('ws_timer', '100ms');
