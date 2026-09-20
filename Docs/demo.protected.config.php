ff865acd7c3e515ec88140d73cec9cb3bb0a9087
gizmore <gizmore@wechall.net>
2026-09-20 16:38:21 +0200

    '-rev error mail config confusion fixed.'

--- Docs/demo.protected.config.php
+++ Docs/demo.protected.config.php
@@ -39,7 +39,7 @@ define('GDO_CONSOLE_VERBOSE', env('GDO_CONSOLE_VERBOSE', true));
 define('GDO_ERROR_LEVEL', env('GDO_ERROR_LEVEL', 0x37ff));
 define('GDO_ERROR_STACKTRACE', env('GDO_ERROR_STACKTRACE', true));
 define('GDO_ERROR_DIE', env('GDO_ERROR_DIE', true));
-define('GDO_ERROR_MAILS', env('GDO_ERROR_MAILS', true));
+define('GDO_SEND_ERROR_MAILS', env('GDO_SEND_ERROR_MAILS', true));
 
 ################
 ### Database ###
